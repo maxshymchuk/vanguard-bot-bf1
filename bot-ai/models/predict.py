66cb07ddefe66646c28ab6c3d5da309d6fbd1396
mystzone44 <mystzone44@gmail.com>
2024-11-11 14:41:33 +0000

    fixes for first build

--- bot-ai/models/predict.py
+++ bot-ai/models/predict.py
@@ -1,26 +1,28 @@
-from imageai.Classification.Custom import CustomImageClassification
 import json
+from PIL import Image
+import onnxruntime as ort
 import numpy as np
-import os
+from models.preprocess import Preprocessor
 
-weight_file_name = "models/resnet50-bf1ai-test_acc_0.99896_epoch-86.pt"
+weight_file_name = "models/bf1ai.onnx"
 json_file_name = "models/bf1ai_model_classes.json"
-number_of_classes = 0
 
-prediction = None
+class Classifier:
+    def __init__(self):
+        with open(json_file_name) as f:
+            self.classes = json.load(f)
+            self.number_of_classes = len(self.classes.keys())
+        self.ort_session = ort.InferenceSession(weight_file_name)
 
-def load_model():
-    global number_of_classes, prediction
-    prediction = CustomImageClassification()
-    prediction.setModelTypeAsResNet50()
-    dir = os.getcwd()
-    prediction.setModelPath(os.path.join(dir, weight_file_name))
-    prediction.setJsonPath(os.path.join(dir, json_file_name))
-    prediction.loadModel()
+        self.preprocesser = Preprocessor()
+        print('Icon model loaded')
 
-    with open(json_file_name) as f:
-        classes = json.load(f)
-        number_of_classes = len(classes.keys())
-
-def predict_icon(image):
-    return prediction.classifyImage(image, result_count=number_of_classes)
+    def predict_icon(self, image):
+        img = Image.fromarray(image).convert("RGB")
+        img = self.preprocesser.preprocess(img)
+        
+        outputs = self.ort_session.run(None, {"input": img})
+        exp_scores = np.exp(outputs[0] - np.max(outputs[0]))
+        probabilities = exp_scores / np.sum(exp_scores)
+        top_prediction = np.argmax(probabilities)
+        return self.classes[str(top_prediction)], probabilities[0][top_prediction]
